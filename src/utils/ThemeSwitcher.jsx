ca19e5c5c050f96722688b1b375092fa2bfbd59d
PavanMeka09 <Mekapavan99@gmail.com>
2024-07-31 17:03:47 +0530

    Add input fields txt

--- src/utils/ThemeSwitcher.jsx
+++ src/utils/ThemeSwitcher.jsx
@@ -1,48 +1,46 @@
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
-import { useState, useEffect} from "react";
-import { motion, useAnimation } from 'framer-motion';
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export const ThemeSwitcher = () => {
-    const [isDark, setIsDark] = useState(
-      localStorage.getItem("isDark") === "true"
-    );
-    
-    useEffect(() => {
-      if (isDark) {
-        document.querySelector("html").classList.add("dark");
-      } else {
-        document.querySelector("html").classList.remove("dark");
-      }
-    }, [isDark]);
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem("isDark") === "true"
+  );
 
-    
-    const controls = useAnimation();
-    
-    const startAnimation = () => {
-      controls.start({
-        scale: 200
-      });
-    };
-    
-    const toggle = () => {
-      localStorage.setItem("isDark", !isDark);
-      setIsDark((prev) => !prev);
-      startAnimation();
-    };
-    
+  useEffect(() => {
     if (isDark) {
-      return (
-        <button onClick={toggle}>
-          <MdOutlineLightMode className="text-3xl text-zinc-100 active:translate-y-1 transition-[transform]" />
-        </button>
-      );
+      document.querySelector("html").classList.add("dark");
     } else {
-      return (
-        <button onClick={toggle} className='relative'>
-          <MdOutlineDarkMode className="text-3xl text-zinc-950 active:translate-y-1 transition-[transform]" />
-          <motion.div initial={{ scale: 0 }} animate={controls} className='h-[100rem] w-[100rem] z-[-99999999] bg-zinc-800 absolute top-[50%] bottom-[50%] translate-x-[-50%] translate-y-[-50%] rounded-full'></motion.div>
-        </button>
-      );
+      document.querySelector("html").classList.remove("dark");
     }
-}
+  }, [isDark]);
+
+  const toggle = () => {
+    localStorage.setItem("isDark", !isDark);
+    setIsDark((prev) => !prev);
+  };
+
+  if (isDark) {
+    return (
+      <button onClick={toggle}>
+        <MdOutlineLightMode className="text-3xl text-zinc-100 active:translate-y-1 transition-[transform]" />
+      </button>
+    );
+  } else {
+    return (
+      <button onClick={toggle} className="flex items-center justify-center">
+          <MdOutlineDarkMode className="text-3xl text-zinc-950 active:translate-y-1 transition-[transform]  rounded-full absolute" />
+          <AnimatePresence>
+          {isDark && (
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              className="h-[1rem] w-[1rem] -z-[9999999] bg-zinc-800 rounded-full absolute"
+            ></motion.div>)}
+          </AnimatePresence>
+        </button>
+    );
+  }
+};
